bb4688ac02935d93c12db7226f014976b23cf091
fadelmuhammad <fadelmuhammad0504@gmail.com>
2017-04-04 12:33:25 +0700

    fix seleksi syarat

--- application/models/model_seleksi_syarat.php
+++ application/models/model_seleksi_syarat.php
@@ -5,15 +5,13 @@ class model_seleksi_syarat extends ci_model{
 		$this->db->insert('seleksi_syarat',$data);
 	}
 	
-	function seleksi_syarat(){
-        return $this->db->query("select pegawai.id_pegawai,pegawai.nama,
-            profil_syarat_pegawai.nilai_pangkat as nilai_pangkat_pegawai, profil_syarat_pegawai.nilai_pendidikan as nilai_pendidikan_pegawai,  
-          profil_syarat_jabatan.nilai_pangkat as nilai_pangkat_jabatan, profil_syarat_jabatan.nilai_pendidikan as nilai_pendidikan_jabatan,
-(select nilai_pangkat_pegawai - nilai_pangkat_jabatan) as gap_pangkat, (select nilai_pendidikan_pegawai - nilai_pendidikan_jabatan) as gap_pendidikan,
-(select gap_pangkat + gap_pendidikan) as gap
-			from jabatan_kosong join profil_syarat_jabatan
-              on jabatan_kosong.id_jabatankosong=profil_syarat_jabatan.id_jabatankosong join pegawai join profil_syarat_pegawai 
-              on pegawai.id_pegawai=profil_syarat_pegawai.id_pegawai group by pegawai.id_pegawai")->result();
+	function seleksi_syarat($id_jabatankosong){
+        return $this->db->query("select pegawai.id_pegawai,pegawai.nama, profil_syarat_pegawai.nilai_pangkat as nilai_pangkat_pegawai,
+        profil_syarat_pegawai.nilai_pendidikan as nilai_pendidikan_pegawai, profil_syarat_jabatan.nilai_pangkat as nilai_pangkat_jabatan,
+        profil_syarat_jabatan.nilai_pendidikan as nilai_pendidikan_jabatan, (select nilai_pangkat_pegawai - nilai_pangkat_jabatan) as gap_pangkat,
+        (select nilai_pendidikan_pegawai - nilai_pendidikan_jabatan) as gap_pendidikan,
+        (select gap_pangkat + gap_pendidikan) as gap from jabatan_kosong join profil_syarat_jabatan on jabatan_kosong.id_jabatankosong=profil_syarat_jabatan.id_jabatankosong
+         join pegawai join profil_syarat_pegawai on pegawai.id_pegawai=profil_syarat_pegawai.id_pegawai where jabatan_kosong.id_jabatankosong='$id_jabatankosong' group by pegawai.id_pegawai")->result();
     }
 
     function insert_hasil_seleksi_syarat($data){
